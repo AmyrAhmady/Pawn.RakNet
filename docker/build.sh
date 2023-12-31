0cfb73083bd66b41cb3d3c8f6b7a7b7536042a19
iAmir <hhm6@yahoo.com>
2023-12-31 05:02:39 +0330

    add docker files to build with ubuntu18

---
+++ docker/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Available configs: Debug, [RelWithDebInfo], Release
+[[ -z "$CONFIG" ]] \
+&& config=RelWithDebInfo \
+|| config="$CONFIG"
+
+docker build \
+    -t open.mp/build:ubuntu-18.04 ./ \
+|| exit 1
+
+folders=('build' 'conan')
+for folder in "${folders[@]}"; do
+    if [[ ! -d "./${folder}" ]]; then
+        mkdir ${folder}
+    fi
+    sudo chown -R 1000:1000 ${folder} || exit 1
+done
+
+docker run \
+    --rm \
+    -t \
+    -w /code \
+    -v $PWD/..:/code \
+    -v $PWD/build:/code/build \
+    -v $PWD/conan:/home/user/.conan \
+    -e CONFIG=${config} \
+    open.mp/build:ubuntu-18.04
